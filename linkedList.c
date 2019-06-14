764cceb0e959b854a350c763722ebb17dd1fed44
midaef <danil.present@mail.ru>
2019-06-14 09:49:21 +0700

    Added delete last element and index

--- linkedList.c
+++ linkedList.c
@@ -5,33 +5,48 @@
 typedef struct list
 {
 	int data;
+	int index;
 	struct list *next;
 } list;
 
-void push_linked_list(int );
-void print_linked_list();
-struct list* temp = NULL;
+list* temp = NULL;
+int key = 0;
 
 void push_linked_list(int value) {
 	list *head = (list*) malloc(sizeof(list));
 	head->data = value;
+	head->index = key;
+	key++;
 	head->next = temp;
 	temp = head;
 }
 
 void print_linked_list() {
-	struct list *ptr = temp;
+	list *ptr = temp;
 	while(ptr != NULL) {
-		printf("%d->", ptr->data);
+		printf("[E:%d,I:%d]->", ptr->data, ptr->index);
 		ptr = ptr->next;
 	}
+	printf("\n");
 }
 
+list* delete_last_element() {
+	list* temp_link = temp;
+	temp = temp->next;
+	key--;
+	return temp_link;
+}
 
 int main(int argc, char const *argv[]) {
 	push_linked_list(40);
+	print_linked_list();
 	push_linked_list(50);
+	print_linked_list();
 	push_linked_list(60);
+	print_linked_list();
+	delete_last_element();
+	print_linked_list();
+	push_linked_list(70);
 	print_linked_list();
 	return 0;
 }
